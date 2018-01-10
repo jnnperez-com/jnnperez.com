f281eeab1c91b763ba11b974d92b31e30d46a96a
JONATAN PEREZ DEL ANGEL <jonatanperezsoy@gmail.com>
2018-01-10 12:55:53 -0600

    contact

--- js/freelancer.js
+++ js/freelancer.js
@@ -35,3 +35,20 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+$('form#contact_form').validate({
+          messages: { },
+          submitHandler: function(form) {
+            $.ajax({
+              url: "https://formspree.io/jonatanperez83@gmail.com",
+              method: "POST",
+              data: $(form).serialize(),
+              dataType: "json",
+              success: function(data) {
+                  $("form#contact_form :input").prop("disabled", true);
+                  $('#thanks').show();
+              }
+            });
+            return false;
+          }
+});
